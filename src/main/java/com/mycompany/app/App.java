7ce37097c2a89986c06c508c89f5e436db74a4cd
lolxdmlg42069 <92958860+lolxdmlg42069@users.noreply.github.com>
2022-09-30 19:43:01 -0400

    sus

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,11 +2,37 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
+import java.util.*;
 
 package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int fortune = 1 + rand.nextInt(10);
+        if ( fortune == 1 ) {
+            System.out.println("my sources tell me that you are sus");
+        } else if ( fortune == 2 ) {
+            System.out.println("are you gay!?");
+        } else if ( fortune == 3 ) {
+            System.out.println("b-baka!");
+        } else if ( fortune == 4 ) {
+            System.out.println("amogus");
+        } else if ( fortune == 5 ) {
+            System.out.println("SUSSY SUS SUS?");
+        } else if ( fortune == 6 ) {
+            System.out.println("im too tired to write the rest of the code");
+        } else if ( fortune == 7 ) {
+            System.out.println("im too tired to write the rest of the code");
+        } else if ( fortune == 8 ) {
+            System.out.println("im too tired to write the rest of the code");
+        } else if ( fortune == 9 ) {
+            System.out.println("im too tired to write the rest of the code");
+        } else if ( fortune == 10 ) {
+            System.out.println("im too tired to write the rest of the code");
+        } else {
+            System.out.println("STOP TRYING TO HACK MY PROGRAM");
+        }
     }
 }
