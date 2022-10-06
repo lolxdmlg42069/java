9e9b3f7a8614cb8c4c33837ba27592c2db69a053
lolxdmlg42069 <92958860+lolxdmlg42069@users.noreply.github.com>
2022-10-06 12:33:08 -0400

    amongus2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -18,60 +18,83 @@ public class App {
         if ( status == 0 ) {
             if ( income <= 8350 ) {
                 tax = income - ( income * 0.10 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 33950 ) {
                 tax = income - ( income * 0.15 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 82250 ) {
                 tax = income - ( income * 0.25 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 171550 ) {
                 tax = income - ( income * 0.28 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 372950 ) {
                 tax = income - ( income * 0.33 );
+                System.out.println("Tax is " + tax);
             } else if ( income >= 372951 ) {
                 tax = income - ( income * 0.35 );
+                System.out.println("Tax is " + tax);
             }
         } else if ( status == 1 ) {
             if ( income <= 16700 ) {
                 tax = income - ( income * 0.10 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 67900 ) {
                 tax = income - ( income * 0.15 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 137050 ) {
                 tax = income - ( income * 0.25 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 208850 ) {
                 tax = income - ( income * 0.28 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 372950 ) {
                 tax = income - ( income * 0.33 );
+                System.out.println("Tax is " + tax);
             } else if ( income >= 372951 ) {
                 tax = income - ( income * 0.35 );
+                System.out.println("Tax is " + tax);
             }
         } else if ( status == 2 ) {
             if ( income <= 8350 ) {
                 tax = income - ( income * 0.10 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 33950 ) {
                 tax = income - ( income * 0.15 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 68525 ) {
                 tax = income - ( income * 0.25 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 104425 ) {
                 tax = income - ( income * 0.28 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 186475 ) {
                 tax = income - ( income * 0.33 );
+                System.out.println("Tax is " + tax);
             } else if ( income >= 186476 ) {
                 tax = income - ( income * 0.35 );
+                System.out.println("Tax is " + tax);
             }
         } else if ( status == 3 ) {
             if ( income <= 11950 ) {
                 tax = income - ( income * 0.10 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 45500 ) {
                 tax = income - ( income * 0.15 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 117450 ) {
                 tax = income - ( income * 0.25 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 190200 ) {
                 tax = income - ( income * 0.28 );
+                System.out.println("Tax is " + tax);
             } else if ( income <= 372950 ) {
                 tax = income - ( income * 0.33 );
+                System.out.println("Tax is " + tax);
             } else if ( income >= 372951 ) {
                 tax = income - ( income * 0.35 );
+                System.out.println("Tax is " + tax);
             }
         }
-        System.out.println("Tax is " + tax);
     }
 }
